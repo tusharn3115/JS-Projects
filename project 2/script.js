0fcfcf25013ca6cedb78c32b5cb02b022d0a0ff3
Tushar Negi <negitushar81@gmail.com>
2024-09-04 19:24:54 +0530

    made the logic for calculating bmi

--- project 2/script.js
+++ project 2/script.js
@@ -9,6 +9,13 @@ form.addEventListener('submit', (e)=>{
     const result = document.querySelector("#results");
 
     if(height === '' || height < 0 || isNaN(height)){
-       result.innerHTML =  "Please give a valid height !!";
+       result.innerHTML =  `Please give a valid height ${height} !!`;
+    }else if(weight === '' || weight < 0 || isNaN(weight)){
+       result.innerHTML =  `Please give a valid weight ${weight} !!`;
+    }else{
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2); // toFixed(2) is if decimal valaue set it to 2 decimal places
+
+        // showing result
+        result.innerHTML = `<span>${bmi}</span>`;
     }
 });
