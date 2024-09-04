d4a1462f70564548cd0ccdba00df4ea3b159b79e
Tushar Negi <negitushar81@gmail.com>
2024-09-04 19:09:08 +0530

    adding logic to the program (calculating bmi)

--- project 2/script.js
+++ project 2/script.js
@@ -1,2 +1,14 @@
 // PROJECT 2 -----------------------------------------------------------------------
 
+const form = document.querySelector("form");
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();   // stoped action on submitting form
+
+    const height = parseInt(document.querySelector("#height").value);
+    const weight = parseInt(document.querySelector("#weight").value);
+    const result = document.querySelector("#results");
+
+    if(height === '' || height < 0 || isNaN(height)){
+       result.innerHTML =  "Please give a valid height !!";
+    }
+});
