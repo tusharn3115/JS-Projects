c397cccf662ae4a5c7c473f033751584edf33d86
Tushar Negi <negitushar81@gmail.com>
2024-09-04 18:42:50 +0530

    made design for the project

---
+++ script.js
@@ -0,0 +1,11 @@
+const buttons = document.querySelectorAll(".button");
+// console.log(buttons);
+
+const body = document.querySelector("body");
+
+buttons.forEach((btn)=>{
+    // console.log(btn);
+    btn.addEventListener("click", (e)=>{
+        // console.log(e);
+    })
+})
