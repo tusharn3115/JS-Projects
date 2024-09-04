d78105aad3fd21ba908139e5d55f00878c6d6f36
Tushar Negi <negitushar81@gmail.com>
2024-09-04 19:41:02 +0530

    small digital time project

--- project 3/script.js
+++ project 3/script.js
@@ -1,1 +1,9 @@
 // PROJECT 3 --------------------------------------------------------------------------------
+
+const clock = document.querySelector("#clock")
+
+setInterval(function(){
+  let date = new Date();
+//   console.log(date.toLocaleTimeString());
+  clock.innerHTML = date.toLocaleTimeString();
+},1000)
