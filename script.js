75df66cc0cd7bc5e86012abe4a930ff79d9a61ea
Tushar Negi <negitushar81@gmail.com>
2024-09-04 18:47:54 +0530

    project 1 completed in which when we click on the color the background color changes as per the color, project good for practicing add event listner

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// PROJECT 1 ----------------------------------------------------------------------------
+
 const buttons = document.querySelectorAll(".button");
 // console.log(buttons);
 
@@ -6,6 +8,18 @@ const body = document.querySelector("body");
 buttons.forEach((btn)=>{
     // console.log(btn);
     btn.addEventListener("click", (e)=>{
-        // console.log(e);
+        // console.log(e.target);
+        if(e.target.id === 'grey'){
+            body.style.backgroundColor = e.target.id;
+        }
+        else if(e.target.id === 'white'){
+            body.style.backgroundColor = e.target.id;
+        }
+        else if(e.target.id === 'blue'){
+            body.style.backgroundColor = e.target.id;
+        }
+        else if(e.target.id === 'yellow'){
+            body.style.backgroundColor = e.target.id;
+        }
     })
 })
